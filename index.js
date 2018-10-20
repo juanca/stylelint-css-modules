79a39be77d448e5da5caf0e192a0a05af830541d
Juan Carlos Medina <juanca.med@gmail.com>
2018-10-19 17:14:13 -0700

    Initial implement of CSS composition rule: ensure each composed class name exists in the targeted file

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
+const fs = require('fs');
+const path = require('path');
 const stylelint = require('stylelint');
 
-module.exports = stylelint.createPlugin('css-modules/test', (options) => {
-  console.log('options', options);
+module.exports = stylelint.createPlugin('css-modules/test', (primaryOption, secondaryOptionObject) => {
+  const options = secondaryOptionObject || { resolve: undefined };
+  const resolve = options.resolve || { alias: {}, modules: [] };
+  const aliasMap = resolve.alias || {};
+  const aliases = Object.keys(aliasMap);
+  const modules = resolve.modules || [];
+
+  function resolveFilePath(contextPath, filePath) {
+    if (filePath[0] === '.') {
+      return path.join(contextPath, filePath);
+    } else {
+      // Copy webpack resolution algorithm -- is this a module I can just import?
+      let resolvedFilePath = filePath;
+      const rootDirectory = resolvedFilePath.split(path.sep)[0];
+      const rootAlias = aliases.find(alias => alias === rootDirectory);
+
+      if (rootAlias) {
+        resolvedFilePath = path.join(aliasMap[rootAlias], ...resolvedFilePath.split(path.sep).slice(1))
+        if (fs.existsSync(resolvedFilePath)) {
+          return resolvedFilePath;
+        }
+      }
+
+      resolvedFilePath = modules
+        .map(module => path.join(module, resolvedFilePath))
+        .find(maybePath => fs.existsSync(maybePath));
+
+      return resolvedFilePath || filePath;
+    }
+  }
 
   return (root, result) => {
-    console.log('root', root);
-    console.log('result', result);
+    const contextPath = path.dirname(result.opts.from);
 
     root.walkDecls((decl) => {
-      console.log('decl', decl);
+      if (decl.prop === 'composes') {
+        const classNames = decl.value.split('from')[0].replace(/\s+/g, ' ').trim().split(' ');
+        const fromExpression = decl.value.split('from')[1].trim();
+
+        if (fromExpression === 'global') {
+          return;
+        }
+
+        const fromFilePath = resolveFilePath(contextPath, fromExpression.slice(1, -1));
+        const fileContents = fs.readFileSync(fromFilePath, 'utf-8');
+
+        classNames
+          .filter(className => !fileContents.includes(`.${className}`))
+          .forEach(className => stylelint.utils.report({
+            index: decl.lastEach,
+            message: utils.ruleMessages('css-modules/test', {
+              expected: selector => `Unable to find ${className} in ${fromFilePath}`,
+            }),
+            node: decl,
+            result: result,
+            ruleName: 'css-modules/test',
+          }));
+      }
     });
   };
 });
