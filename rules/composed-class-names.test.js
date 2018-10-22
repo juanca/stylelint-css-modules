245a8ebad1e190ae831c67ae56260bc81df1f0b2
Juan Carlos Medina <juanca.med@gmail.com>
2018-10-22 15:21:49 -0700

    First test: ensure Jest can fail when Stylelint fails

--- rules/composed-class-names.test.js
+++ rules/composed-class-names.test.js
@@ -1,21 +1,19 @@
 const stylelint = require('stylelint');
 const pattern = require('./composed-class-names.js');
 
-stylelint.lint({
-  config: {
-    plugins: [
-      './composed-class-names.js',
-    ],
-    rules: {
-      'css-modules/composed-class-names': [true, {}],
+test('the test can fail due to internal stylelint errors', () => {
+  return stylelint.lint({
+    config: {
+      plugins: [
+        './composed-class-names.js',
+      ],
+      rules: {
+        'css-modules/composed-class-names': [true, {}],
+      },
     },
-  },
-  configBasedir: 'rules',
-  files: ['fixtures/composed-class-names/*.css'],
-}).then(function (resultObject) {
-  console.log('then', resultObject);
-}).catch(function (error) {
-  console.log('Something went wrong. See below for details.');
-  console.log(error);
-  process.exit(1);
+    configBasedir: 'rules',
+    files: ['fixtures/composed-class-names/*.css'],
+  }).then(function (resultObject) {
+    console.log('then', resultObject);
+  });
 });
