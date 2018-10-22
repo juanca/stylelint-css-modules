c9f36ab3a3dadf63b3357c9411c8efc06306f470
Juan Carlos Medina <juanca.med@gmail.com>
2018-10-22 15:50:54 -0700

    Remove redundant test ensuring Jest works with Stylelint node API

--- rules/composed-class-names.test.js
+++ rules/composed-class-names.test.js
@@ -1,23 +1,6 @@
 const stylelint = require('stylelint');
 const pattern = require('./composed-class-names.js');
 
-test('the test can fail due to internal stylelint errors', () => {
-  return stylelint.lint({
-    config: {
-      plugins: [
-        './composed-class-names.js',
-      ],
-      rules: {
-        'css-modules/composed-class-names': [true, {}],
-      },
-    },
-    configBasedir: 'rules',
-    files: ['fixtures/composed-class-names/*.css'],
-  }).then(function (resultObject) {
-    console.log('then', resultObject);
-  });
-});
-
 test('the plugin parses correct compose statements', () => {
   return stylelint.lint({
     config: {
